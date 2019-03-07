b1e0115cca88a405fb8dca92dbf01ce6c79700f3
Daniella Noren <daniellanoren@live.se>
2019-03-07 02:50:28 +0100

    Initial commit

---
+++ src/main/java/daniella/iths/se/librarystorageservice/LibraryStorageServiceApplication.java
@@ -0,0 +1,13 @@
+package daniella.iths.se.librarystorageservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryStorageServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryStorageServiceApplication.class, args);
+    }
+
+}
